29195216c9431dadd4b0792c65b0e6c7299f3b80
carloslopez777 <lopeztorres0704@gmail.com>
2025-11-22 16:39:22 -0700

    Practicas

--- 2.04.proyecto_final/modelos/cancion.py
+++ 2.04.proyecto_final/modelos/cancion.py
@@ -4,18 +4,20 @@ import pandas as pd
 def agregarCancion():
     nombre = input("Nombre de la canción: ")
     artista = input("Nombre del artista: ")
-    duracion = input("Duración: ")  
+    duracion = input("Duración: ") 
+    streams = input("Streams: ") 
     cancion = {
         "nombre": nombre,
         "artista": artista,
-        "duracion": duracion
+        "duracion": duracion,
+        "streams": streams
     }
     return cancion
 
 def mostrarCanciones(playlist):
     print(Fore.LIGHTMAGENTA_EX + " ===Lista de canciones=== " + Style.BRIGHT)
     for i, cancion in enumerate(playlist["canciones"]):
-        print(f"ID:{i} - {cancion["nombre"]} by ({cancion["artista"]}), [{cancion["duracion"]}]")
+        print(f"ID:{i} - {cancion["nombre"]} by {cancion["artista"]}, [{cancion["duracion"]}], [{cancion["streams"]}]")
 
 def eliminarCancion(playlist, idPlaylist):
     playlist.pop(idPlaylist)
